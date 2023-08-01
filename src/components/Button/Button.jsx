a9b326bd50e8be7c1d386ef70f49852cca95cbc9
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-08-01 03:49:26 +0300

    UPDATE:modernizing and cleaning the code

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
 
-export const Button = ({ text = 'Follow', toggle }) => {
+export const Button = ({ text = 'Follow', toggle, disabled }) => {
   const backgroundColorClass = text === 'Follow' ? 'bg-[#EBD8FF]' : 'bg-[#5CD3A8]';
 
   return (
     <button
+      disabled={disabled}
       onClick={toggle}
       type="button"
       className={` ${backgroundColorClass} flex justify-center items-center w-196 h-50 rounded-[10.311px] shadow-[-2.5776965618133545px 6.873857021331787px 20.621572494506836px 0px rgba(0, 0, 0, 0.23) text-[#373737] uppercase text-lg font-semibold`}
@@ -15,6 +16,7 @@ export const Button = ({ text = 'Follow', toggle }) => {
 };
 
 Button.propTypes = {
+  disabled: PropTypes.bool.isRequired,
+  text: PropTypes.string.isRequired,
   toggle: PropTypes.func.isRequired,
-  text: PropTypes.string,
 };
--- src/components/TweetCard/TweetCard.jsx
+++ src/components/TweetCard/TweetCard.jsx
@@ -1,28 +1,22 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
 import PropTypes from 'prop-types';
 
-import { toggleFollowersStatus } from '@/services/api/tweetsApi';
+import { useToggleFollowersStatus } from '@/hooks/useToggleFollowersStatus';
 
 import { Button } from '@/components/Button/Button';
 
 export const TweetCard = ({ data: { id, user, avatar, tweets, followers, isFollowed } }) => {
-  const queryClient = useQueryClient();
-
-  const calcFollowers = isFollowed ? followers - 1 : followers + 1;
-
-  const { mutate: toggle } = useMutation({
-    mutationFn: () => toggleFollowersStatus(id, !isFollowed, calcFollowers),
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['tweets'] });
-    },
-  });
+  // Використовуємо хук useToggleFollowersStatus для отримання мутації toggle та стану завантаження isLoading
+  const {
+    toggle: { mutate: toggle },
+    isLoading,
+  } = useToggleFollowersStatus(id, isFollowed, followers);
 
   return (
     <li className="flex flex-col justify-center  items-center w-380  h-460 rounded-3xl bg-gradient-to-br from-custom-blue1 via-custom-blue2 to-custom-blue3 shadow-[-2.5776965618133545px 6.873857021331787px 20.621572494506836px 0px rgba(0, 0, 0, 0.23)]">
       <img src={avatar} alt={user} />
       <p>{tweets}</p>
       <p>{followers}</p>
-      <Button toggle={toggle} text={isFollowed ? 'Following' : 'Follow'} />
+      <Button toggle={toggle} text={isFollowed ? 'Following' : 'Follow'} disabled={isLoading} />
     </li>
   );
 };
