769859cd49f36e1e6bb6c5f35eb149fdc8780044
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-08-02 14:41:20 +0300

    UPDATE:No Tweets Found

--- src/components/NoTweetsFound/NoTweetsFound.jsx
+++ src/components/NoTweetsFound/NoTweetsFound.jsx
@@ -1,5 +1,6 @@
 export const NoTweetsFound = () => (
-  <div className="flex h-[calc(100vh-80px)] items-center justify-center p-5 w-full">
+  <div className="flex flex-col h-[calc(100vh-80px)] items-center justify-center p-5 w-full">
     <h2 className="mt-5 text-[36px] font-bold text-slate-800 lg:text-[50px]">No Tweets Found</h2>
+    <p>Choose a different filter category to display tweets!!!</p>
   </div>
 );
