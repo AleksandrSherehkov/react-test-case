e6b4e83ae3796764eec6af0f6941ae9ce3cf5c7c
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-08-02 15:02:22 +0300

    Update:Select styled

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -10,7 +10,7 @@ export const Dropdown = ({ options, selectedOption, onSelectOption }) => {
         tabIndex="0"
         value={selectedOption}
         onChange={e => onSelectOption(e.target.value)}
-        className="text-[#EBD8FF] font-semibold bg-[#5736A3] rounded-lg p-2 cursor-pointer"
+        className="text-[#EBD8FF] font-semibold border-none outline-none bg-[#5736A3] rounded-lg p-2 cursor-pointer"
       >
         {options.map(({ value, label }) => (
           <option key={value} value={value} className="font-semibold italic">
