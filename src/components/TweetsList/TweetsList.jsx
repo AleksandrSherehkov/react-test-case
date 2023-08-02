50770573fe24332fcf9254d3f15570b19ff690a0
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-08-02 14:25:12 +0300

    UPDATE:styled

--- src/components/TweetsList/TweetsList.jsx
+++ src/components/TweetsList/TweetsList.jsx
@@ -12,23 +12,24 @@ import { FilterResult } from '@/components/FilterResult/FilterResult';
 const TWEETS_PER_PAGE = 3;
 
 export const TweetsList = ({ state }) => {
-  const { isLoading, error, data: users } = useTweetsQuery(state);
+  const { isLoading, error, data: filteredTweets } = useTweetsQuery(state);
   const { perPage, loadMore } = useLoadMore(TWEETS_PER_PAGE);
-
-  const showUsers = users.slice(0, perPage);
-  const isShowBtn = users.length > showUsers.length;
 
   if (isLoading) return <LoadingIndicator />;
 
   if (error) return <ErrorIndicator error={error} />;
 
-  if (users.length === 0) {
+  if (!filteredTweets || filteredTweets.length === 0) {
     return <NoTweetsFound />;
   }
 
+  const showUsers = filteredTweets.slice(0, perPage);
+
+  const isShowBtn = filteredTweets.length > showUsers.length;
+
   return (
     <>
-      <FilterResult users={users} />
+      <FilterResult users={filteredTweets} />
       <ul className="flex  justify-center items-center flex-wrap gap-[48px] w-full mt-20">
         {showUsers.map(user => (
           <TweetCard key={user.id} data={user} />
@@ -39,7 +40,7 @@ export const TweetsList = ({ state }) => {
         <button
           type="button"
           onClick={loadMore}
-          className="flex justify-center items-centertext-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 transition-all duration-300 ease-in-out hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 m-auto mt-10 "
+          className="flex justify-center items-centertext-white text-[#EBD8FF] bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 transition-all duration-300 ease-in-out hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 m-auto mt-10 "
         >
           Load More
         </button>
